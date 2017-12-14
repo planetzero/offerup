9281c98a9eadef29e8fa579b30f62b0ee9dd1430
Maksym Blank <blank.maksim@yandex.com>
2017-12-13 20:10:26 -0600

    remove node_modules

--- node_modules/best-encoding/History.md
+++
@@ -1,8 +0,0 @@
-
-# 0.1.1
-
-  - Fixed deps
-
-# 0.1.0
-
-  - Initial release
--- node_modules/node-geocoder/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var GeocoderFactory = require('./lib/geocoderfactory.js');
-
-var Exports = GeocoderFactory.getGeocoder.bind(GeocoderFactory);
-
-module.exports =  Exports;
